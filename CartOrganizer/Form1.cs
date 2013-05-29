4b52e3a251df8ce00537f858e24584a3d1d9d0ef
Ryan Weir <ryweir@gmail.com>
2013-05-28 20:11:51 -0400

    Added empty project for cart organizer

---
+++ CartOrganizer/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CartOrganizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TODO: get the input list of names (should already be grouped together prior to input)
+            //TODO: get the number of holes on the golf course (names should be independent of the index of the hole, just in case there needs to be more starters at that part of the course)
+            //TODO: distribute the carts across the course, export the information to the word template (still need to receive a copy of this template)
+        }
+    }
+}
